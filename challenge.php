343734c96f75fe1838e4271f542420230b781a7f
Henry <h.ngosytchev@gmail.com>
2021-11-10 11:21:52 +0100

    initiate files for exercises

---
+++ challenge.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Day-3: Exrcises 1-4</title>
+</head>
+<body>
+    
+
+<!-- We will create a Restaurant Database (you can create it directly in php MyAdmin).
+
+The database should contain one table called dishes with columns dishID, image(URL link), name, price and meal description. If there are any columns that you think could compliment your project feel free to expand. 
+
+You should be able to:
+
+Display all meals
+
+Add a meal
+
+Delete a meal -->
+
+</body>
+</html>
